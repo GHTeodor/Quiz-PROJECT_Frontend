d82bfcb78c1170f9b9106dc98baee6aa3225e2b2
GHTeodor <Theodor.Lviv@gmail.com>
2022-09-20 00:21:58 +0300

    FormGroup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'users', pathMatch: 'full' },
-  { path: 'users', loadChildren: () => import('./modules').then(m => m.UsersModule) },
-  { path: 'questions', loadChildren: () => import('./modules').then(m => m.QuestionsModule) },
-  { path: 'answers', loadChildren: () => import('./modules').then(m => m.AnswerModule) }
+  // { path: '', redirectTo: 'users', pathMatch: 'full' },
+  // { path: 'users', loadChildren: () => import('./modules').then(m => m.UsersModule) },
+  // { path: 'questions', loadChildren: () => import('./modules').then(m => m.QuestionsModule) },
+  // { path: 'answers', loadChildren: () => import('./modules').then(m => m.AnswerModule) }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,21 @@ import { HttpClientModule } from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { FormComponent } from './components/form/form/form.component';
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FormComponent
   ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        AppRoutingModule,
+        ReactiveFormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
